91528e018a07d61e65a915fbf44d6c0e047f5815
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-03-29 11:04:56 +0530

    Update huggingface.js

--- config/huggingface.js
+++ config/huggingface.js
@@ -1,5 +1,18 @@
+//require("@huggingface/inference") returns an object
+//From that object, we take InferenceClient
+//We are importing a class(InferenceClient) that helps us talk to Hugging Face AI.
 const { InferenceClient } = require("@huggingface/inference");
-
-const client = new InferenceClient(process.env.HUGGING_FACE_API_KEY);
+// We create an instance (object) of InferenceClient
+// We pass an API key
+// process.env.HUGGING_FACE_API_KEY
+// Reads the key from .env
+// Keeps it secure (not written in code)
+const client = new InferenceClient(process.env.HUGGING_FACE_API_KEY);//creating object
 
 module.exports = client;
+
+// Why are we creating an object?
+
+// Because InferenceClient is a class, not a ready-made object.
+
+// So you must create an instance (object) to actually use it.
